a80ad423178d568e2b15cc62325b8a4efe7cfc01
der-basti <devnull@der-basti.com>
2012-06-05 20:07:03 +0200

    Update ToDoS

--- src/de/th/wildau/se2/date20120426/ToDoS.java
+++ src/de/th/wildau/se2/date20120426/ToDoS.java
@@ -5,7 +5,9 @@ public class ToDoS {
 	// Enums
 	// switch - break, continue
 	// verdeckte Datenfelder
+	// collections (list, set, map, ...)
 	// Komplexe Objekte - shedow/deep copy, clone
 	// packages - Max fragen!
 	// ... algorithmen
+	// design patterns
 }
